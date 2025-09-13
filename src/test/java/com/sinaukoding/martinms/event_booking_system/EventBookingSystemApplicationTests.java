45e0b3f27eb5ea85a03eafb7cd28b025ba7f0c37
Martin Mulyo Syahidin <mulyosyahidin95@gmail.com>
2025-09-13 23:31:18 +0700

    Event Booking System - Initial Commit

---
+++ src/test/java/com/sinaukoding/martinms/event_booking_system/EventBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinaukoding.martinms.event_booking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
